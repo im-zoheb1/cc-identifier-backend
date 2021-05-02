e11e337d3bedb49f26349f6276f07a1e349416c1
Zuhaib Khan <xuhaib002@gmail.com>
2021-05-03 04:01:57 +0500

    signup code refactored

--- code/app.py
+++ code/app.py
@@ -3,11 +3,20 @@ from flask_restful import Resource, Api
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
-from user import UserRegister
+from resources.user import UserRegister
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app.secret_key = 'ccidentifier'
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 # JWT creates a new endpoint /auth
 jwt = JWT(app, authenticate, identity)
@@ -21,4 +30,7 @@ class CCIdentifier(Resource):
 api.add_resource(CCIdentifier, '/something')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
--- code/security.py
+++ code/security.py
@@ -1,20 +1,13 @@
-from user import User
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
 
-users = [
-    User(1, 'bob', 'asdf')
-]
 
-username_mapping = { u.username: u for u in users }
-
-userid_mapping = { u.id: u for u in users}
-
-# authenticating the user
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    if user and user.password == password:
+    user = UserModel.find_by_username(username)
+    if user and safe_str_cmp(user.password, password):
         return user
-    
-# identity takes the payload, which is the content of jwt-token
+
+
 def identity(payload):
-    user_id = payload['identity'] # getting the user id from that payload# getting the user id from that payload
-    return userid_mapping.get(user_id, None)
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
